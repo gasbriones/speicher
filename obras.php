d031151935c31ddfd2e733315e65802d2d6251c0
Gaston Briones <gbriones@cmd.com.ar>
2015-04-17 01:04:05 -0300

    fix

--- obras.php
+++ obras.php
@@ -3,19 +3,18 @@ $_SESSION['active'] = "obras";
 ?>
 <!DOCTYPE html>
 <?php include 'layout/head.php' ?>
-<body class="obras">
+<body class="resena">
 <?php include 'layout/header.php' ?>
 <div class="main wrapper">
-    <section class="carousel">
-        <ul class="slider">
-            <li><img src="images/slider-1.jpg" /></li>
-            <li><img src="images/slider-2.jpg" /></li>
-            <li><img src="images/slider-3.jpg" /></li>
-        </ul>
+    <section class="content">
+        <article>
+            <h4>Obras</h4>
+            <p>
+
+                "La mayoría de mis obras representan mujeres porque creo en la belleza innata del género femenino. Pero también pinto personajes masculinos, aggiornados con un glamour exaltado, bien pop. Me gusta encontrar belleza en la fealdad, en la deformidad, porque creo que todos somos un poco freaks y diferentes en el fondo"
+            </p>
+        </article>
     </section>
 </div>
-<script src="js/libs/jquery-2.1.3.min.js"></script>
-<script src="js/libs/jquery.bxslider.min.js"></script>
-<script src="js/init.js"></script>
 </body>
 </hmtl>
