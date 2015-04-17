2b218df2d06c845f1b22c813012fca0cecbda3ed
Gaston Briones <gbriones@cmd.com.ar>
2015-04-16 23:43:14 -0300

    fi

---
+++ obras.php
@@ -0,0 +1,21 @@
+<?php
+$_SESSION['active'] = "obras";
+?>
+<!DOCTYPE html>
+<?php include 'layout/head.php' ?>
+<body class="obras">
+<?php include 'layout/header.php' ?>
+<div class="main wrapper">
+    <section class="carousel">
+        <ul class="slider">
+            <li><img src="images/slider-1.jpg" /></li>
+            <li><img src="images/slider-2.jpg" /></li>
+            <li><img src="images/slider-3.jpg" /></li>
+        </ul>
+    </section>
+</div>
+<script src="js/libs/jquery-2.1.3.min.js"></script>
+<script src="js/libs/jquery.bxslider.min.js"></script>
+<script src="js/init.js"></script>
+</body>
+</hmtl>
